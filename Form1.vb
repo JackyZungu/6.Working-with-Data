cb2ae8d1e7dc522f640eedec5e69cdfa2a5eb035
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-04 10:01:06 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim YourMessage As String
+        'Dim age As Integer
+        Dim birthYear As Integer
+
+        YourMessage = "happy Birthday! how old are you now"
+        ' InputBox("Enter age")
+
+        MsgBox(YourMessage)
+
+        birthYear = Val(InputBox("Enter birth year"))
+
+        MsgBox("your were born on year" & 2023 - 1996)
+
+        'MsgBox("You are now" & birthYear - 2023 & "years old")
+    End Sub
+End Class
